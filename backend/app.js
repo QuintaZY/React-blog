3d917c54d45b73afa7fdf51a14ae3aa4e27b62f1
ZhangYX <2455636304@qq.com>
2022-12-20 13:09:04 +0800

    后端中间件

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ const initDB = require('./src/init/initDB')
 const initServer = require('./src/init/initServer')
 const initRoute = require('./src/init/initRoute')
 const cors = require('cors')
+const morgan = require('morgan')
+
 const express = require('express')
 const app = express()
 // 中间件
-app.use(cors) // 跨域
-app.use(express.json) // 解析json 数据
-
+app.use(cors({credential: true, origin: true})) // 跨域
+app.use(express.json()) // 解析json 数据
+app.use(morgan('tiny')) // http 日志
 // 路由
 initRoute(app)
 
